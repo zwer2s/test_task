960e9ecfecff0ce02dd44213afc9ce30a47ebb0b
rsavdonin <rsavdonin@avito.ru>
2024-06-12 16:14:16 +0400

    added 1 blank line and translated all comments to English

--- main.py
+++ main.py
@@ -4,19 +4,18 @@ import os
 
 COMPARE_FILE_PATH = "data/product_prices_calculated.parquet"
 
-def get_all_products(limit=30):
-    base_url = "https://dummyjson.com/products"
+
+def get_all_products(base_url="https://dummyjson.com/products", limit=30, skip=0):
     all_products = []
-    skip = 0
     total_products = 0
 
-    # Получаем общее количество продуктов
+    # Getting the amount of items
     response = requests.get(f"{base_url}?limit=1")
     if response.status_code == 200:
         data = response.json()
         total_products = data['total']
 
-    # Итерируемся по страницам данных
+    # Iterating through the pages of data
     while skip < total_products:
         response = requests.get(f"{base_url}?limit={limit}&skip={skip}")
         if response.status_code == 200:
@@ -30,12 +29,13 @@ def get_all_products(limit=30):
 
 
 def extract_relevant_fields(products):
-    a = 1
+    def calculate_final_price(product):
+        return round(product['price'] - product['price'] * product['discountPercentage'] / 100, 2)
     return [
         {
             "title": product["title"],
             "id": product["id"],
-            "final_price": round(product['price'] - product['price'] * product['discountPercentage'] / 100, 2)
+            "final_price": calculate_final_price(product)
         }
         for product in products
     ]
@@ -51,21 +51,21 @@ def save_to_parquet(data, directory, file_name):
 
 
 def find_most_expensive_product(file_path):
-    # Прочитать данные из файла Parquet
+    # Getting data from parquet file
     df = pd.read_parquet(file_path, engine='pyarrow')
 
-    # Найти строку с максимальной ценой
+    # Looking for an item with the biggest price
     most_expensive_product = df.loc[df['final_price'].idxmax()]
 
     return most_expensive_product.to_dict()
 
 
 def find_missing_data(reference_file_path, compare_file_path):
-    # Прочитать данные из файлов Parquet
+    # Getting data from parquet files
     reference_df = pd.read_parquet(reference_file_path, engine='pyarrow')
     compare_df = pd.read_parquet(compare_file_path, engine='pyarrow')
 
-    # Найти недостающие данные
+    # Searching for missing items
     merged_df = reference_df.merge(compare_df, on=['id', 'title'], how='left', indicator=True)
     missing_data = merged_df[merged_df['_merge'] == 'left_only'].drop(columns=['_merge'])
 
@@ -73,28 +73,28 @@ def find_missing_data(reference_file_path, compare_file_path):
 
 
 def count_matching_prices(reference_file_path, compare_file_path):
-    # Прочитать данные из файлов Parquet
+    # Getting data from parquet files
     reference_df = pd.read_parquet(reference_file_path, engine='pyarrow')
     compare_df = pd.read_parquet(compare_file_path, engine='pyarrow')
 
-    # Найти совпадающие данные
+    # Searching for the equal data
     merged_df = reference_df.merge(compare_df, on=['id', 'title', 'final_price'], how='inner')
 
-    # Посчитать количество совпадающих товаров по цене
+    # Counting rows of equal data
     matching_count = merged_df.shape[0]
 
     return matching_count
 
 
 def execute_task(compare_file_path=COMPARE_FILE_PATH):
-    # Запрос продуктов
+    # Getting all products
     products = get_all_products()
-    # Выборка необходимых данных
+    # Getting required data from products and calculating it
     relevant_data = extract_relevant_fields(products)
-    # Сохранение данных в parquet файл
+    # Saving data to parquet file
     reference_file_path = save_to_parquet(relevant_data, "./data", "products_new.parquet")
 
-    # 1 - Поиск самого дорогого товара
+    # 1 - Looking the most expensive item
     print("1 - What product is the most expensive according to actual data?")
     most_expensive_product = find_most_expensive_product(reference_file_path)
     print(f"The most expensive is: {most_expensive_product['title']}")
