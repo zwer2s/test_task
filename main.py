38d6f1439ae01d5f0177ec37748c0539a97de6c4
rsavdonin <rsavdonin@avito.ru>
2024-06-12 16:32:13 +0400

    Added all docstrings

--- main.py
+++ main.py
@@ -6,6 +6,13 @@ COMPARE_FILE_PATH = "data/product_prices_calculated.parquet"
 
 
 def get_all_products(base_url="https://dummyjson.com/products", limit=30, skip=0):
+    """
+    Getting the products list with all details from the link
+    :param base_url: (str) link where products are listed
+    :param limit: (int) amount of products to get
+    :param skip: (int) how many products to skip
+    :return: (list) list of products with details
+    """
     all_products = []
     total_products = 0
 
@@ -29,6 +36,11 @@ def get_all_products(base_url="https://dummyjson.com/products", limit=30, skip=0
 
 
 def extract_relevant_fields(products):
+    """
+    Extracting relevant fields from products to another list
+    :param products: (list) list of products
+    :return: (list) list of relevant fields
+    """
     def calculate_final_price(product):
         return round(product['price'] - product['price'] * product['discountPercentage'] / 100, 2)
     return [
@@ -42,6 +54,13 @@ def extract_relevant_fields(products):
 
 
 def save_to_parquet(data, directory, file_name):
+    """
+    Saving data to parquet file
+    :param data: (list) data to save
+    :param directory: (str) path to directory where data should be saved
+    :param file_name: (sta) file name where data should be saved
+    :return: (str) path to the file where data will be saved
+    """
     if not os.path.exists(directory):
         os.makedirs(directory)
     file_path = os.path.join(directory, file_name)
@@ -51,6 +70,11 @@ def save_to_parquet(data, directory, file_name):
 
 
 def find_most_expensive_product(file_path):
+    """
+    Finding the most expensive product
+    :param file_path: (str) path to file with required data
+    :return: (dict) dictionary with most expensive product
+    """
     # Getting data from parquet file
     df = pd.read_parquet(file_path, engine='pyarrow')
 
@@ -61,6 +85,12 @@ def find_most_expensive_product(file_path):
 
 
 def find_missing_data(reference_file_path, compare_file_path):
+    """
+    Comparing 2 data files and looking for missing rows in one of them
+    :param reference_file_path: (str) path to reference file
+    :param compare_file_path: (str) path to file for comparison
+    :return: (pandas.core.frame.DataFrame) dataframe with missing data
+    """
     # Getting data from parquet files
     reference_df = pd.read_parquet(reference_file_path, engine='pyarrow')
     compare_df = pd.read_parquet(compare_file_path, engine='pyarrow')
@@ -73,6 +103,12 @@ def find_missing_data(reference_file_path, compare_file_path):
 
 
 def count_matching_prices(reference_file_path, compare_file_path):
+    """
+    Counting how many products match in 2 files
+    :param reference_file_path: (str) path to reference file
+    :param compare_file_path: (str) path to file for comparison
+    :return: (int) number of products match in 2 files
+    """
     # Getting data from parquet files
     reference_df = pd.read_parquet(reference_file_path, engine='pyarrow')
     compare_df = pd.read_parquet(compare_file_path, engine='pyarrow')
@@ -87,6 +123,11 @@ def count_matching_prices(reference_file_path, compare_file_path):
 
 
 def execute_task(compare_file_path=COMPARE_FILE_PATH):
+    """
+    Function for visually displaying the results of an interview task
+    :param compare_file_path: (str) path to file with expected data
+    :return: None
+    """
     # Getting all products
     products = get_all_products()
     # Getting required data from products and calculating it
